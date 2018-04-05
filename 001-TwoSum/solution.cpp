a30c805db8ccad8c8f7033fc47e7657f89eddeb1
bingo <iambingonju@gmail.com>
2018-04-05 17:10:43 +0800

    001 two sum

---
+++ 001-TwoSum/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+		for (int i = 0; i < nums.size() - 1; i ++) {
+			int another = target - nums[i];
+			vector<int>::iterator iter = find(nums.begin() + i + 1, nums.end(), another);
+			if (iter != nums.end()) {
+				int another_index = iter - nums.begin();
+				return { i, another_index };
+			}
+		}
+
+		return { 0, 0 };
+    }
+};
