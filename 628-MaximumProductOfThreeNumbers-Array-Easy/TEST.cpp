a5d648f237834563fb8c4b9cc0329545bd74d6a3
bingo <iambingonju@gmail.com>
2018-10-18 00:17:14 +0800

    628 Maximum product of three numbers

---
+++ 628-MaximumProductOfThreeNumbers-Array-Easy/TEST.cpp
@@ -0,0 +1,24 @@
+
+#include "../catch/catch.hpp"
+#include "solution.cpp"
+
+// Using [catch] to test your code
+// For more details, go https://github.com/catchorg/Catch2/blob/master/docs/test-cases-and-sections.md#top
+
+// Set test name and tags here
+TEST_CASE("Test name", "[Tag1][Tag2]...")
+{
+    Solution s;
+
+	//
+	// example:
+    // std::vector<int> v1{2, 7, 11, 15};
+    // REQUIRE( (s.twoSum(v1, 9) == std::vector<int>{0, 1}) );
+	//
+	// Edit your test code here
+	vector<int> v1 = { 1, 2, 3 };
+	REQUIRE( s.maximumProduct(v1) == 6 );
+
+	vector<int> v2 = { 1, 2, 3, 4 };
+	REQUIRE( s.maximumProduct(v2) == 24 );
+}
