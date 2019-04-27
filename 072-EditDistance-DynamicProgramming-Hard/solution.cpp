9cd5b5280e6ba1d7550996cbd5f8f293caf39882
bingo <iambingonju@gmail.com>
2019-04-27 15:32:13 +0800

    072 Edit distance (optimized)

--- 072-EditDistance-DynamicProgramming-Hard/solution.cpp
+++ 072-EditDistance-DynamicProgramming-Hard/solution.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 class Solution {
 public:
-	// Edit your solution code here
+	// O(nm) space version
+	/*
 	int minDistance(string word1, string word2) {
 		int len1 = word1.size() + 1, len2 = word2.size() + 1;
 		vector<vector<int>> distance(len1, vector<int>(len2));
@@ -15,11 +16,33 @@ public:
 				} else if (word1[i - 1] == word2[j - 1]) {
 					distance[i][j] = distance[i - 1][j - 1];
 				} else {
-					distance[i][j] = min(min(distance[i][j - 1] + 1, distance[i - 1][j] + 1), distance[i - 1][j - 1] + 1);
+					distance[i][j] = min(min(distance[i][j - 1], distance[i - 1][j]), distance[i - 1][j - 1]) + 1;
 				}
 			}
 		}
 		return distance[len1 - 1][len2 - 1];
+	}
+	*/
+
+	// O(m) space version
+	int minDistance(string word1, string word2) {
+		int len1 = word1.size() + 1, len2 = word2.size() + 1, lt;
+		vector<int> lastLine(len2);
+		for (int i = 0; i < len2; i ++) lastLine[i] = i;
+		for (int i = 1; i < len1; i ++) {
+			lt = lastLine[0];
+			lastLine[0] = i;
+			for (int j = 1; j < len2; j ++) {
+				int tmp = lastLine[j];
+				if (word1[i - 1] == word2[j - 1]) {
+					lastLine[j] = lt;
+				} else {
+					lastLine[j] = min(lt, min(lastLine[j - 1], lastLine[j])) + 1;
+				}
+				lt = tmp;
+			}
+		}
+		return lastLine[len2 - 1];
 	}
 };
 
